6a05e464b9192b5ec51411479e6ee0cb4ef2b371
Jaime Alba Pastor <jalbapastor@gmail.com>
2023-01-30 11:27:40 +0100

    Create serial.py

---
+++ Code/NVIDIA/serial.py
@@ -0,0 +1,28 @@
+import glob
+import sys
+import serial
+
+def findPort():
+    """Find the serial port that the Arduino is connected to.
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+    
+    result = []
+    
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
