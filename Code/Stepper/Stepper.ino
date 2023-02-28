ac9bd7bb76d7f47116b80114c432048bd529fd3d
Brice-Mabille <114212325+Brice-Mabille@users.noreply.github.com>
2023-02-28 16:25:14 +0100

    Create Stepper.ino

---
+++ Code/Stepper/Stepper.ino
@@ -0,0 +1,20 @@
+const int Pas = 3;
+const int Dir = 2;
+
+
+void setup() {
+Serial.begin(115200);
+pinMode(Pas,OUTPUT);
+pinMode(Dir,OUTPUT);
+digitalWrite(Dir,HIGH);
+}
+
+void loop() {
+for(int x = 0; x < 600; x++) { ///x = nombre de tours
+digitalWrite(Pas,HIGH);
+delayMicroseconds(500);
+digitalWrite(Pas,LOW);
+delay(4);
+}
+delay(1000);
+}
