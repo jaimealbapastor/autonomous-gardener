d02f22f0ea190caa3821275a6624ad6acec5a7b2
Brice-Mabille <114212325+Brice-Mabille@users.noreply.github.com>
2023-04-15 10:55:20 +0200

    S20

--- Code/Servo/Servo.ino
+++ Code/Servo/Servo.ino
@@ -12,29 +12,57 @@ const int pin3 = 19;
 const int pin4 = 18;
 int pos = 0;  // variable to store the servo position
 
+void rotation(){
+    for (pos = 0; pos <= 120; pos++) {  // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    Serial.println(pos);
+    myservo.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(30);  // waits 15ms for the servo to reach the position
+  }
+  for (pos = 120; pos >= 0; pos--) {  // goes from 180 degrees to 0 degrees
+    Serial.println(pos);
+    myservo.write(pos);  // tell servo to go to position in variable 'pos'
+    delay(30);  // waits 15ms for the servo to reach the position
+  }
+}
+
 void setup() {
 
   Serial.begin(9600);
-  for (int i = 0; i <= 3; i++){
-    pinMode(pin[i],OUTPUT);
-    myservo[i].attach(pin[i]);          // attaches the servo on pin1 9 to the servo object
-  } 
+  pinMode(pin1, OUTPUT);
+  ///pinMode(pin2,OUTPUT);
+  ///pinMode(pin3,OUTPUT);
+  //pinMode(pin4,OUTPUT);
+  myservo.attach(pin4);  // attaches the servo on pin1 9 to the servo object
+  //myservo2.attach(pin2);
+  //myservo3.attach(pin3);
+  //myservo4.attach(pin4);
+  myservo.write(0);
 }
 
 void loop() {
-  for (pos = 0; pos <= 360; pos += 1) {  // goes from 0 degrees to 180 degrees
+rotation();
+
+}
+
+void txrfg() {
+  for (pos = 0; pos <= 270; pos++) {  // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     Serial.println(pos);
-    for (int j = 0; j <= 3; j++){
-      myservo[j].write(pos);      // tell servo to go to position in variable 'pos'
-    }
-    delay(30);           // waits 15ms for the servo to reach the position
+    myservo.write(pos);  // tell servo to go to position in variable 'pos'
+    myservo2.write(pos);
+    myservo3.write(pos);
+    myservo4.write(pos);
+    delay(30);  // waits 15ms for the servo to reach the position
   }
-  for (pos = 360; pos >= 0; pos -= 1) {  // goes from 180 degrees to 0 degrees
+  for (pos = 360; pos >= 0; pos--) {  // goes from 180 degrees to 0 degrees
     Serial.println(pos);
-    for (int j = 0; j <= 3; j++){
-      myservo[j].write(pos);     // tell servo to go to position in variable 'pos'
+    for (int j = 0; j <= 3; j++) {
+      myservo.write(pos);  // tell servo to go to position in variable 'pos'
+      myservo2.write(pos);
+      myservo3.write(pos);
+      myservo4.write(pos);
     }
-    delay(30);      // waits 15ms for the servo to reach the position
+    delay(30);  // waits 15ms for the servo to reach the position
   }
 }
---
+++ Reports/Brice/Séance 20.md
@@ -0,0 +1,27 @@
+## Rapports de séance Brice Mabille
+
+- 11/04/2023 : Setup du projet GitHub partagé
+
+# Séance 20
+
+During this session, I worked on the electronics of the robot.
+
+Indeed, I wanted to make the servo motors in function so I asked for the converter and I received some which could help me to test it.
+
+But those converter are not the final one, there were just the testing converter to see if the the servos can do what they have to do.
+
+It took me a lot of time because I wanted to make 2 servos in parallel. 
+
+Actually it worked well.
+
+To go further, I changed the offset of the servos with Jaime to allow the robot moving in rotation and changing his direction. 
+
+We arrived to do it to 2 servos. 
+
+I changed the code of the servos because the precedent didn't work.
+
+It is registered in the code "Servo".
+
+![img](../../Documentation/Images/ROBOT3.jpeg)
+
+For the next session, I will finish the servos functioning, and I will adapt the electronics in the frame because there are too much cables so it is not ideal.
