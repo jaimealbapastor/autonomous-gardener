4bbc164a5f5fb629ab5cd45e7205dbd916ff43d6
Jaime Alba Pastor <jalbapastor@gmail.com>
2023-04-28 18:27:01 +0200

    fablab

--- Code/Movement/Movement.ino
+++ Code/Movement/Movement.ino
@@ -9,11 +9,9 @@
  * @param arrayList
  * @param size
  */
-void printSignedList(int8_t *arrayList, int size)
-{
+void printSignedList(int8_t *arrayList, int size) {
   Serial.print("[ ");
-  for (int i = 0; i < size; i++)
-  {
+  for (int i = 0; i < size; i++) {
     Serial.print(*(arrayList + i));
     if (i != size - 1)
       Serial.print(", ");
@@ -27,11 +25,9 @@ void printSignedList(int8_t *arrayList, int size)
  * @param arrayList
  * @param size
  */
-void printUnsignedList(uint8_t *arrayList, int size)
-{
+void printUnsignedList(uint8_t *arrayList, int size) {
   Serial.print("[ ");
-  for (int i = 0; i < size; i++)
-  {
+  for (int i = 0; i < size; i++) {
     Serial.print(*(arrayList + i));
     if (i != size - 1)
       Serial.print(", ");
@@ -39,41 +35,66 @@ void printUnsignedList(uint8_t *arrayList, int size)
   Serial.print(" ]");
 }
 
-uint8_t nbMotors = 6;
+const uint8_t nbMotors = 6;
 
-uint8_t dir_pins[6] = {1, 2, 3, 4, 5, 6};    // pins for direction
-uint8_t pwd_pins[6] = {7, 8, 9, 10, 11, 12}; // pins for speed
+const uint8_t right_pwm_pin = 5;
+const uint8_t right_dir_pin = 2;
+const uint8_t left_pwm_pin = 4;
+const uint8_t left_dir_pin = 3;
+
+uint8_t dir_pins[6] = { left_dir_pin, right_dir_pin,
+                        left_dir_pin, right_dir_pin,
+                        left_dir_pin, right_dir_pin };  // pins for direction
+
+uint8_t pwd_pins[6] = { left_pwm_pin, right_pwm_pin,
+                        left_pwm_pin, right_pwm_pin,
+                        left_pwm_pin, right_pwm_pin };  // pins for speed
 
 WheelController w(dir_pins, pwd_pins);
 
-void test_1()
-{
+void test_1() {
   Serial.println(" === Test 1 | Changing speeds and directions ===");
 
   static const uint8_t nbMotorsChange = 3;
-  uint8_t someMotors[nbMotorsChange] = {0, 2, 4};     // motors to change speed
-  int8_t new_speeds[nbMotorsChange] = {-75, 50, -25}; // new speeds for motors
+  uint8_t someMotors[nbMotorsChange] = { 0, 2, 4 };      // motors to change speed
+  int8_t new_speeds[nbMotorsChange] = { -75, 50, -25 };  // new speeds for motors
 
   Serial.print("Speed: ");
-  printUnsignedList(w.getSpeeds(), nbMotors); // display speed for all motors
+  printUnsignedList(w.getSpeeds(), nbMotors);  // display speed for all motors
 
   Serial.print("\nDirection: ");
-  printSignedList(w.getDirections(), nbMotors); // display direction for all motors
+  printSignedList(w.getDirections(), nbMotors);  // display direction for all motors
 
-  w.setSpeed(someMotors, new_speeds, nbMotorsChange); // set speed for some motors
+  w.setSpeed(someMotors, new_speeds, nbMotorsChange);  // set speed for some motors
 
   Serial.print("\nModified speed: ");
-  printUnsignedList(w.getSpeeds(), nbMotors); // display modified speed for all motors
+  printUnsignedList(w.getSpeeds(), nbMotors);  // display modified speed for all motors
   Serial.print("\nModified directions: ");
-  printSignedList(w.getDirections(), nbMotors); // display modified direction for all motors
+  printSignedList(w.getDirections(), nbMotors);  // display modified direction for all motors
 }
 
-void setup()
-{
+void test_class() {
+  w.forward(100);
+  delay(2000);
+  w.stop();
+  delay(2000);
+  w.backward(100);
+  delay(2000);
+  w.stop();
+  delay(2000);
+}
+
+void setup() {
   Serial.begin(9600);
 
-  test_1();
+  pinMode(right_pwm_pin, OUTPUT);
+  pinMode(right_dir_pin, OUTPUT);
+  pinMode(left_pwm_pin, OUTPUT);
+  pinMode(left_dir_pin, OUTPUT);
+
+  
 }
-void loop()
-{
+void loop() {
+  
+  test_class();
 }
