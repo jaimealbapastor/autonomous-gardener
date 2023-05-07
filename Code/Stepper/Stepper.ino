b39db0f1f20b013ec59c26a107c0ab7ff25fda68
Brice-Mabille <114212325+Brice-Mabille@users.noreply.github.com>
2023-05-07 16:12:47 +0200

    stash

--- Code/Stepper/Stepper.ino
+++ Code/Stepper/Stepper.ino
@@ -1,54 +1,63 @@
-const int Pas = 3;        //STEPPER
-const int Dir = 2;
+const int StepperPas = 2;  //STEPPER
+const int StepperDir = 3;
 
-int MoteurDIR1 = 10;    //DC
-int MoteurPW1 = 9;
+int MoteurDIR1 = 9;  //DC
+int MoteurPW1 = 10;
 
 
 
 void setup() {
-Serial.begin(115200);
+  Serial.begin(9600);
 
-pinMode(Pas,OUTPUT);    //STEPPER
-pinMode(Dir,OUTPUT);
-digitalWrite(Dir,HIGH);
+  pinMode(StepperPas, OUTPUT);  //STEPPER
+  pinMode(StepperDir, OUTPUT);
+  digitalWrite(StepperDir, HIGH);
 
-pinMode(MoteurDIR1, OUTPUT);    //DC
-pinMode(MoteurPW1, OUTPUT);
+  pinMode(MoteurDIR1, OUTPUT);  //DC
+  pinMode(MoteurPW1, OUTPUT);
 }
 
-void AvancerStepper(){
-for(int x = 0; x < 600; x++) { ///x = nombre de tours
-digitalWrite(Pas,HIGH);
-delayMicroseconds(500);
-digitalWrite(Pas,LOW);
-delay(4);
-}
-delay(1000);
+void AvancerStepper() {
+  digitalWrite(StepperDir, HIGH);
+  for (int x = 0; x < 6000; x++) {  ///x = nombre de tours
+    digitalWrite(StepperPas, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(StepperPas, LOW);
+    delay(4);
+  }
 }
 
-void ReculerStepper(){
-
+void ReculerStepper() {
+  digitalWrite(StepperDir, LOW);
+  for (int x = 0; x < 6000; x++) {  ///x = nombre de tours
+    digitalWrite(StepperPas, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(StepperPas, LOW);
+    delayMicroseconds(50);
+  }
 }
 
 void AvancerDC() {
 
-  analogWrite(MoteurDIR1, 0);  ///tourner vers l'avant moteur
-  analogWrite(MoteurPW1, 127);
+  digitalWrite(MoteurDIR1, 1);  ///tourner vers l'avant moteur
+  analogWrite(MoteurPW1, 255);
+  delay(3000);
+  analogWrite(MoteurPW1, 0);
 }
 
 void ReculerDC() {
 
-  analogWrite(MoteurDIR1, 127);  ///tourner vers l'arrière moteur
-  analogWrite(MoteurPW1, 127);
+  digitalWrite(MoteurDIR1, 0);  ///tourner vers l'arrière moteur
+  analogWrite(MoteurPW1, 255);
+  delay(3000);
+  analogWrite(MoteurPW1, 0);
 }
 
 
 void loop() {
   //if robot à l'arrêt alors
-  AvancerStepper();
-  AvancerDC();
-  delay(4000);
-  ReculerDC();
+  //AvancerStepper();
+  //AvancerDC();
   ReculerStepper();
+  delay(4000);  
 }
